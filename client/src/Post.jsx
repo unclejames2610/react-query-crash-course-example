d317663db5bbdcdaf4e7550fb792215389d8bb73
unclejames2610 <udealaebuka@gmail.com>
2024-09-02 16:49:24 +0100

    chore:check

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,22 +1,24 @@
-import { useQuery } from "@tanstack/react-query"
-import { getPost } from "./api/posts"
-import { getUser } from "./api/users"
+import { useQuery } from "@tanstack/react-query";
+import { getPost } from "./api/posts";
+import { getUser } from "./api/users";
 
 export default function Post({ id }) {
   const postQuery = useQuery({
     queryKey: ["posts", id],
     queryFn: () => getPost(id),
-  })
+  });
 
   const userQuery = useQuery({
     queryKey: ["users", postQuery?.data?.userId],
     enabled: postQuery?.data?.userId != null,
     queryFn: () => getUser(postQuery.data.userId),
-  })
+  });
 
-  if (postQuery.status === "loading") return <h1>Loading...</h1>
+  console.log(postQuery);
+
+  if (postQuery.status === "loading") return <h1>Loading...</h1>;
   if (postQuery.status === "error") {
-    return <h1>{JSON.stringify(postQuery.error)}</h1>
+    return <h1>{JSON.stringify(postQuery.error)}</h1>;
   }
 
   return (
@@ -33,5 +35,5 @@ export default function Post({ id }) {
       </h1>
       <p>{postQuery.data.body}</p>
     </>
-  )
+  );
 }
